1d3a27360852486a4a5a0ef51d64f0a3d59dc056
ismailcanaydin <ismailcanaydin@gmail.com>
2022-12-01 16:03:18 +0300

    bir veriyi güncellemek

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, incrementByAmount } from '../redux/counter/counterSlice'
 
 function Counter() {
+    const [amount, setAmount] = useState(0)
     const countValue = useSelector(state => state.counter.value)
+    const dispatch = useDispatch()
 
-    console.log(countValue);
   return (
     <div>
         <h1>{countValue}</h1>
+
+        <button onClick={() => dispatch(decrement())}>Decrement</button>&nbsp;
+        <button onClick={() => dispatch(increment())}>Increment</button>
+
+        <br />
+        <br />
+        <input type={'number'} value={amount} onChange={(e) => setAmount(e.target.value)} /> 
+        <button onClick={() => dispatch(incrementByAmount(amount))}>Increment by Amount</button>
     </div>
   )
 }
